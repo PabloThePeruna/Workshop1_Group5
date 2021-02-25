9c123ab51934fc5aeb66a4e27cbb0f5f49a9156f
Katjuska <katariina.peltopihko@gmail.com>
2021-02-25 12:43:20 +0200

    SampleScene: testi
    Torvia on nyt 4. Hiirellä klikkaamalla pinkit ja harmaa torvi antavat eri äänen. Pöydän paikkaa siirretty.

---
+++ Workshop1_Group5/Assets/Models/Tuuttaus.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tuuttaus : MonoBehaviour
+{
+    AudioSource ac;
+    public AudioClip myclip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ac = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void OnMouseDown()
+    {
+        ac.PlayOneShot(myclip);
+    }
+}
