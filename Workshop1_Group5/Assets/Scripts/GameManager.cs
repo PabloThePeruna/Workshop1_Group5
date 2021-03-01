63e98024117d78dc2df36e73fa990c28f3fa0612
Jaani Kekäläinen <44496339+jkekala@users.noreply.github.com>
2021-03-01 13:22:12 +0200

    Janttereita laitettu yksi satunnainen toimii
    Janttereita laitettu, yksi satunnainen nousee kun pelin aloittaa

---
+++ Workshop1_Group5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public bool peliAktiivinen;
+    public bool jantteriAktiivinen;
+    public GameObject aloitaButton;
+
+    private float turnSpeed = 2.0f;
+
+
+    public List<GameObject> jantterit = new List<GameObject>();
+
+    private IEnumerator janterSelectCoroutine;
+
+
+    void Start()
+    {
+        aloitaButton.SetActive(true);
+        peliAktiivinen = false;
+
+        janterSelectCoroutine = JanterSelectAjastin(Random.Range(2.0f, 5.0f));
+
+        
+        //Deaktivoi jantterit
+        for (int i = 0; i < jantterit.Count; i++)
+        {
+            jantterit[i].SetActive(false);
+        }
+        
+
+    }
+
+    void Update()
+    {
+        if (peliAktiivinen == true)
+        {
+            StartCoroutine(janterSelectCoroutine);
+
+        }
+
+
+    }
+
+    void JanterAktiivinen()
+    {
+
+    }
+
+    private IEnumerator JanterSelectAjastin(float waitTime)
+    {
+
+        yield return new WaitForSeconds(waitTime);
+
+
+            jantterit[Random.Range(0, jantterit.Count)].SetActive(true);
+            jantteriAktiivinen = true;
+            print("Satunnainen jantteri aktiiviseksi");
+        
+
+
+
+        
+    }
+
+
+    public void AloitaPeli()
+    {
+        aloitaButton.SetActive(false);
+        peliAktiivinen = true;
+    }
+
+}
---
+++ Workshop1_Group5/Assets/Scripts/Janter.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Janter : MonoBehaviour
+{
+
+    private HingeJoint hinge;
+
+    private JointMotor motor;
+
+    private GameManager gameManagerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManagerScript = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+        hinge = GetComponent<HingeJoint>();
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameManagerScript.jantteriAktiivinen == true)
+        {
+            motor.force = 10;
+            motor.targetVelocity = 200;
+            motor.freeSpin = false;
+            hinge.motor = motor;
+            hinge.useMotor = true;
+            print("Pitäisi kääntyä pystyyn");
+
+        }
+
+        else
+        {
+            motor.force = 10;
+            motor.targetVelocity = -200;
+            motor.freeSpin = false;
+            hinge.motor = motor;
+            hinge.useMotor = true;
+            print("Pitäisi kaatua");
+
+        }
+
+        /*
+        //Tähän "kun osuu, niin kaatuu)
+        if (osuma)
+        {
+            motor.force = 10;
+            motor.targetVelocity = -200;
+            motor.freeSpin = false;
+            hinge.motor = motor;
+            hinge.useMotor = true;
+            print("Pitäisi kaatua");
+        }
+        */
+    }
+
+
+
+}
