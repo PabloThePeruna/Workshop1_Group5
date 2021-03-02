9593da2c0a07e3614d74cac442d474b17619c802
Jaani Kekäläinen <44496339+jkekala@users.noreply.github.com>
2021-03-02 18:11:23 +0200

    Lisätty ääniä, canvasjuttuja, restart yms.

--- Workshop1_Group5/Assets/Scripts/GameManager.cs
+++ Workshop1_Group5/Assets/Scripts/GameManager.cs
@@ -2,14 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
 
     public bool peliAktiivinen;
     public bool jantteriAktiivinen;
+
+    private bool playAudio = true;
+    private bool playAudio2 = true;
+    private bool playAudio3 = true;
+    private bool playAudio4 = true;
+
+
     public GameObject aloitaButton;
     public GameObject loppuPanel;
+
+    public AudioClip intro;
+    public AudioClip afterFirstShot;
+    public AudioClip ceaseFire;
+    public AudioClip GO;
+    public AudioClip sec5;
+    public AudioClip sec15;
+    public AudioClip sec30;
+    public AudioClip oneTwoThree;
+
+    public AudioSource leftSpeaker;
+    public AudioSource rightSpeaker;
+
 
     public Text timerText;
     private float alkuAika;
@@ -32,7 +53,13 @@ public class GameManager : MonoBehaviour
         aloitaButton.SetActive(true);
         loppuPanel.SetActive(false);
         peliAktiivinen = false;
-        
+
+        leftSpeaker.PlayOneShot(intro);
+        rightSpeaker.PlayOneShot(intro);
+
+
+
+
         //Deaktivoi jantterit aluksi
         for (int i = 0; i < jantterit.Count; i++)
         {
@@ -57,6 +84,37 @@ public class GameManager : MonoBehaviour
 
         }
 
+        if (alkuAika <= 30)
+        {
+            if (playAudio)
+            {
+                leftSpeaker.PlayOneShot(sec30);
+                rightSpeaker.PlayOneShot(sec30);
+                playAudio = false;
+
+            }
+        }
+
+        if (alkuAika <= 15)
+        {
+            if (playAudio2)
+            {
+                leftSpeaker.PlayOneShot(sec15);
+                rightSpeaker.PlayOneShot(sec15);
+                playAudio2 = false;
+            }
+        }
+
+        if (alkuAika <= 5)
+        {
+            if (playAudio3)
+            {
+                leftSpeaker.PlayOneShot(sec5);
+                rightSpeaker.PlayOneShot(sec5);
+                playAudio3 = false;
+            }
+        }
+
         //Kun aika ja peli loppuu
         if (alkuAika <= 0)
         {
@@ -64,29 +122,60 @@ public class GameManager : MonoBehaviour
             scoreText.text = " ";
             scoreTextEnd.text = "Scores: " + scores.ToString();
             timerText.text = " ";
+            peliAktiivinen = false;
+            jantteriAktiivinen = false;
+
+
+            if (playAudio4)
+            {
+                leftSpeaker.PlayOneShot(ceaseFire);
+                rightSpeaker.PlayOneShot(ceaseFire);
+                playAudio4 = false;
+            }
+
+
+
         }
-                
+
     }
 
     void JanterAktiivinen()
     {
+        if (peliAktiivinen == true)
+        {
+            print("toistaa " + alkuAika.ToString("F0"));
 
-        print("toistaa " + alkuAika.ToString("F0"));
+            jantterit[Random.Range(0, jantterit.Count)].SetActive(true);
+            jantteriAktiivinen = true;
+            print("Satunnainen jantteri aktiiviseksi");
+        }
 
-        jantterit[Random.Range(0, jantterit.Count)].SetActive(true);
-        jantteriAktiivinen = true;
-        print("Satunnainen jantteri aktiiviseksi");
     }
 
 
 
     public void AloitaPeli()
     {
+        leftSpeaker.PlayOneShot(afterFirstShot);
+        rightSpeaker.PlayOneShot(afterFirstShot);
+
         aloitaButton.SetActive(false);
         peliAktiivinen = true;
         //Aktivoi satunnaisia janttereita
+
+
+
         InvokeRepeating("JanterAktiivinen", 2f, 2f);  //Kahden sekunnin alkuviive, toistaa joka toinen sekunti
 
     }
+
+    public void Restart()
+    {
+        SceneManager.GetActiveScene();
+        SceneManager.LoadScene("TahdonOllaLääkintäMies");
+    }
+
+
+
 
 }
