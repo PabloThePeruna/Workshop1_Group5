35d4a26f5587fcf2a4e5511252c9b298c242b6c3
PabloThePeruna <59955623+PabloThePeruna@users.noreply.github.com>
2021-03-02 23:00:24 +0200

    Ase ampuu sormea käyttämällä

--- Workshop1_Group5/Assets/Scripts/Gun.cs
+++ Workshop1_Group5/Assets/Scripts/Gun.cs
@@ -1,25 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
 
 public class Gun : MonoBehaviour
 {
+    public SteamVR_Action_Boolean fireAction;
     public GameObject bullet;
     public Transform barrelPivot;
     public float shootingSpeed = 1;
+
+    private Interactable interactable;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        interactable = GetComponent<Interactable>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        // check if grabbed
+        if(interactable.attachedToHand != null)
         {
-            Fire();
+            SteamVR_Input_Sources source = interactable.attachedToHand.handType;
+
+            if(fireAction[source].stateDown)
+            {
+                Fire();
+            }
         }
     }
 
