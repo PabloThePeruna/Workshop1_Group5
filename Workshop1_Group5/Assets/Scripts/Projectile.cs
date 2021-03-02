0857adeaaafff9dd93aba911c04c819fb987400d
Katariina <katariina.peltopihko@gmail.com>
2021-03-03 01:33:40 +0200

    Effectääni

--- Workshop1_Group5/Assets/Scripts/Projectile.cs
+++ Workshop1_Group5/Assets/Scripts/Projectile.cs
@@ -7,13 +7,14 @@ public class Projectile : MonoBehaviour
 
     private GameManager gameManagerScript;
     private Janter janterScript;
+    private AudioSource source;
 
     // Start is called before the first frame update
     void Start()
     {
         gameManagerScript = GameObject.Find("GameManager").GetComponent<GameManager>();
         janterScript = GameObject.FindGameObjectWithTag("Jantteri").GetComponent<Janter>();
-
+        source = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -29,6 +30,7 @@ public class Projectile : MonoBehaviour
             gameManagerScript.scores++;
             Debug.Log("Osuma");
             janterScript.KaadaJantteri();
+            source.Play();
         }
     }
 }
