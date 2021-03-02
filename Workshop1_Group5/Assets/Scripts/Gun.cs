57fa23bb91b07889a90b6400f96dc03e2b7475bc
PabloThePeruna <59955623+PabloThePeruna@users.noreply.github.com>
2021-03-02 22:48:41 +0200

    Ampuva Ase lisätty

---
+++ Workshop1_Group5/Assets/Scripts/Gun.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform barrelPivot;
+    public float shootingSpeed = 1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        Debug.Log("Fire");
+        Rigidbody bulletrb = Instantiate(bullet, barrelPivot.position, barrelPivot.rotation).GetComponent<Rigidbody>();
+        bulletrb.velocity = barrelPivot.forward * shootingSpeed;
+    }
+}
